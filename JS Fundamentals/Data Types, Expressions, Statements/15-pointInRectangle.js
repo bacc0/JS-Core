8a380c60fe26f21bca38cd56f5c8f5d2c4eed187
Veselin Trayanov <bacco7@me.com>
2018-07-06 23:05:42 +0100

    15

--- JS Fundamentals/Data Types, Expressions, Statements/15-pointInRectangle.js
+++ JS Fundamentals/Data Types, Expressions, Statements/15-pointInRectangle.js
@@ -1,10 +1,19 @@
-function bigThreNums( input ) {
-    
-    let theBiggerNum = Math.max( Number(input[0]), 
-                                 Number(input[1]), 
-                                 Number(input[2]) );
+function point(input) {
 
-    console.log(theBiggerNum);
+           let x = Number(input[0]);
+           let y = Number(input[1]);
+           let xMin = Number(input[2]);
+           let xMax = Number(input[3]);
+           let yMin = Number(input[4]);
+           let yMax = Number(input[5]);
+
+           if (x >= xMin && x <= xMax &&
+               y >= yMin && y <= yMax) {
+
+                      console.log('inside');
+
+           } else
+                      console.log('outside');
 }
 
-bigThreNums([-1, 2, 12, -3, 3]);
+point([-1, 2, 12, -3, 3]);

1313d2cc07a01542c3e680a1a0abf75a72013d68
Veselin Trayanov <bacco7@me.com>
2018-07-07 18:14:48 +0100

    1

---
+++ JS Fundamentals/EXERCISE-FUNCTIONS/04-cookingByNumbers.js
@@ -0,0 +1,33 @@
+function coocking( arr ) {
+    let [start, op1, op2, op3, op4, op5] = 
+    [ Number(arr[0]), arr[1], arr[2], arr[3], arr[4], arr[5] ];
+
+    let chop  = (num) => num / 2;
+    let dice  = (num) => Math.sqrt(num);
+    let spice = (num) => ++num;
+    let bake  = (num) => num * 3;
+    let filet = (num) => num * 0.8;
+
+    for(let op of [op1, op2, op3, op4, op5]){
+         switch (op) {
+           case 'chop':
+                start = chop(start);
+                      break;
+           case 'dice':
+                start = dice(start);
+                      break;
+           case 'spice':
+                start = spice(start);
+                      break;
+           case 'bake':
+                start = bake(start);
+                      break;
+           case 'filet':
+                start = filet(start);
+                      break;
+         }
+         console.log(start);
+     }
+}
+
+coocking(['32', 'chop', 'chop', 'chop', 'chop', 'chop'])

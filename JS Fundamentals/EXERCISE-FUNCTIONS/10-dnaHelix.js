5772cae43ce6581c8549812ac4c677fb31064790
Veselin Trayanov <bacco7@me.com>
2018-07-08 01:02:13 +0100

    10-dnaHelix
    10-dnaHelix

---
+++ JS Fundamentals/EXERCISE-FUNCTIONS/10-dnaHelix.js
@@ -0,0 +1,35 @@
+function helix( number ) {
+
+    let rows = Number(number);
+    let sequence = 'ATCGTTAGGG';
+    let currentIndex = 0;
+    let counter = 0;
+
+    for (let i = 0; i < rows; i++) {
+         let currentRow = i % 4 
+
+         if ( currentIndex === sequence.length ) {
+            currentIndex = 0;
+         }
+
+         if ( currentRow === 0 ) {
+
+             console.log('**' + sequence[currentIndex++] +
+                                sequence[currentIndex++] +
+                         '**');
+         } else if (currentRow === 1 || currentRow === 3) {
+
+             console.log('*' + sequence[currentIndex++] + '--' +
+                               sequence[currentIndex++] +
+                         '*');
+         } else {
+
+             console.log(sequence[currentIndex++] + '-----' +
+                         sequence[currentIndex++]);
+         }
+    }
+
+}
+
+
+helix( 10 )

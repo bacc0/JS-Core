0d2ec8cead7f0ea1a271b48aab6d85692571ef55
Veselin Trayanov <bacco7@me.com>
2018-07-11 19:36:18 +0100

    1

--- JS Fundamentals/01-Data Types, Expressions, Statements/02-areaAndPerimeter.js
+++ JS Fundamentals/01-Data Types, Expressions, Statements/02-areaAndPerimeter.js
@@ -2,8 +2,8 @@ function areaPerimeter(x, y) {
     let perimeter = x * y;
     let area = (x + y) * 2;
     
-    console.log(Number(perimeter.toFixed(2)));
-    console.log(Number(area.toFixed(2)));
+    console.log(perimeter);
+    console.log(area);
 }
 
 areaPerimeter(2, 2);

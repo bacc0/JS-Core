6e40b0a0bf2f6e0962746abfed3bdae01875681b
Veselin Trayanov <bacco7@me.com>
2018-07-12 13:44:09 +0100

    1

--- JS Fundamentals/03-ARRAYS AND MATRICES-Lab/03-negativePositiveNumbers.js
+++ JS Fundamentals/03-ARRAYS AND MATRICES-Lab/03-negativePositiveNumbers.js
@@ -3,8 +3,10 @@ function solve(arr) {
     let result = [];
 
     for (let num of arr) {
+
         if (num < 0) {
             result.unshift(num);
+            
         } else {
             result.push(num);
         }

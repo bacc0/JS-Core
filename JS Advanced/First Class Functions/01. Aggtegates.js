d991d465d97472d1bd5baacce1920462f06e6f11
Veselin Trayanov <bacco7@me.com>
2018-08-12 16:07:35 +0100

    1

--- JS Advanced/First Class Functions/01. Aggtegates.js
+++ JS Advanced/First Class Functions/01. Aggtegates.js
@@ -11,4 +11,15 @@ function reduce(arr, func) {
 console.log(reduce(['3', '4', '5', '6'] , (x, y)=> Number(x) + Number(y)));
 console.log(reduce(['3', '4', '5', '6'] , (x, y)=> Number(x) - Number(y)));
 console.log(reduce(['3', '4', '5', '6'] , (x, y)=> Number(x) * Number(y)));
+
+
 console.log(reduce(['3', '4', '5', '6'] , (x, y)=> '' + x + y));
+
+console.log(
+    reduce(['3', '4', '5', '6'] , 
+        (x, y) => {
+            if( Number(x) > Number(y))
+                return x;
+            else
+                return y;
+}));

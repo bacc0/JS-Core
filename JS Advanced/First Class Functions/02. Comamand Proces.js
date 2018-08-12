68fdbedddea96aa157856f886fee113b2478e907
Veselin Trayanov <bacco7@me.com>
2018-08-12 19:29:15 +0100

    2

--- JS Advanced/First Class Functions/02. Comamand Proces.js
+++ JS Advanced/First Class Functions/02. Comamand Proces.js
@@ -1,6 +1,14 @@
 let commandProcessor = ( function() {
     let text = '';
     return {
-        append: (t) => text = text + t;
+        append: (t) => text = text + t,
+        removeStart: (count) => text = text.slice(count),
+        removeEnd: (count) => text = text.slice(0, text.length - count),
+        print: function (  ) {
+            console.log(text);
+        }
     };
 })();
+
+console.log(commandProcessor.append('pesho'));
+

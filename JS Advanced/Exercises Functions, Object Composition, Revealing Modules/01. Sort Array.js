bca4e4fa1059b4efd2684e5082ed833253783c91
Veselin Trayanov <bacco7@me.com>
2018-08-13 11:57:04 +0100

    01

--- JS Advanced/Exercises Functions, Object Composition, Revealing Modules/01. Sort Array.js
+++ JS Advanced/Exercises Functions, Object Composition, Revealing Modules/01. Sort Array.js
@@ -1,14 +1,14 @@
 function sortArray(array, orderType) {
     if(orderType === 'asc'){
-        console.log(array.sort((a, b) => {
+        return array.sort((a, b) => {
             return a - b;
-        }));
+        });
     } else {
-        console.log(array.sort((a, b) => {
+        return array.sort((a, b) => {
             return b - a;
-        }));
+        });
     }
 }
 
-sortArray([14, 7, 17, 6, 8], 'asc');
-sortArray([14, 7, 17, 6, 8], 'desc');
+console.log(sortArray([14, 7, 17, 6, 8], 'asc'));
+console.log(sortArray([14, 7, 17, 6, 8], 'desc'));

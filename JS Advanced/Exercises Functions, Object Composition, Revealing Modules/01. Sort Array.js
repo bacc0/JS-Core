f7d0f78843660cde8406fa5cdce82e1f33827147
Veselin Trayanov <bacco7@me.com>
2018-08-13 11:46:40 +0100

    1

--- JS Advanced/Exercises Functions, Object Composition, Revealing Modules/01. Sort Array.js
+++ JS Advanced/Exercises Functions, Object Composition, Revealing Modules/01. Sort Array.js
@@ -1,23 +1,14 @@
-function reduce(arr, func) { 
-
-    let result = arr[0];
-
-    for(nextElement of arr.slice(1)){
-        result = func(result, nextElement)
+function sortArray(array, orderType) {
+    if(orderType === 'asc'){
+        console.log(array.sort((a, b) => {
+            return a - b;
+        }));
+    } else {
+        console.log(array.sort((a, b) => {
+            return b - a;
+        }));
     }
-    return result;
 }
 
-console.log(reduce(['3', '4', '5', '6'] , (x, y)=> Number(x) + Number(y)));
-console.log(reduce(['3', '4', '5', '6'] , (x, y)=> Number(x) - Number(y)));
-console.log(reduce(['3', '4', '5', '6'] , (x, y)=> Number(x) * Number(y)));
-console.log(reduce(['3', '4', '5', '6'] , (x, y)=> '' + x + y));
-
-console.log(
-    reduce(['3', '4', '5', '6'] , 
-        (x, y) => {
-            if( Number(x) > Number(y))
-                return x;
-            else
-                return y;
-}));
+sortArray([14, 7, 17, 6, 8], 'asc');
+sortArray([14, 7, 17, 6, 8], 'desc');

37a9af9aa3770201ffbd5f89856e3c50ea0a33a5
Veselin Trayanov <bacco7@me.com>
2018-08-15 14:54:49 +0100

    1

--- JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /01. Agrigate.js
+++ JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /01. Agrigate.js
@@ -7,5 +7,12 @@ function agrigate( arr ) {
             result = func (result, nextElement);
         return result;    
     }
+    console.log('Sum =' + reduce(arr, (a, b) => a + b));
+    console.log('Min =' + reduce(arr, (a, b) => a > b ? b : a));
+    console.log('Max =' + reduce(arr, (a, b) => a > b ? a : b));
+    console.log('Product =' + reduce(arr, (a, b) => a * b));
+    console.log('Join =' + reduce(arr, (a, b) => '' + a + b));
     
 }
+
+let arr = [ 5, 6, 7, 8, 9]

da9a994433776c2b1134e867c04187688365968c
Veselin Trayanov <bacco7@me.com>
2018-08-15 15:18:52 +0100

    2

---
+++ JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /02. formatCurrency.js
@@ -0,0 +1,13 @@
+function formatCurrency( separator, symbol, symbolFirst, value ) {
+    
+    let result = Math.trunc(value) + separator;
+
+    result += value.toFixed(2).substr(-2, 2);
+
+    if(symbolFirst) return symbol + ' ' + result;
+
+    else return result + ' ' + symbol;
+}
+console.log(formatCurrency(',', 'лв', false, 1.60))
+console.log(formatCurrency('.', '€', false, 1200))
+console.log(formatCurrency('.', '$', true, 1.66544))

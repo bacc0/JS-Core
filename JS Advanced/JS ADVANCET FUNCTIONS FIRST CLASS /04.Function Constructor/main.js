e1babecad6bc2f98605a48612f65c31f7fabb815
Veselin Trayanov <bacco7@me.com>
2018-08-16 17:31:11 +0100

    4

--- JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /04.Function Constructor/main.js
+++ JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /04.Function Constructor/main.js
@@ -8,18 +8,25 @@ let Person = function(name, yearOfBirth, job){
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calcukateAge = function(){
-        console.log(2016 - this.yearOfBirth);
-    }
 }
 
-let john = new Person('John', 1990, 'theacher');
 
+Person.prototype.calcukateAge = function(){
+    console.log(2016 - this.yearOfBirth)  //add Methods
+}
+Person.prototype.anatomically = 'Homo sapiens (Homo Erectus)'  //add Property
+
+
+let john = new Person('John', 1990, 'theacher');
 let jane = new Person('Jane', 1969, 'designer');
 let mark = new Person('Mark', 1948, 'retired');
 
 john.calcukateAge();
 jane.calcukateAge();
 mark.calcukateAge();
+
+console.log(john.anatomically);
+console.log(jane.anatomically);
+console.log(mark.anatomically);
 
 

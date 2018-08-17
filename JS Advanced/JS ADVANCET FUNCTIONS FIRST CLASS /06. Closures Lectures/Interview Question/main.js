ded86c944dd02ef42f89f5771ead8b9af8a213ec
Veselin Trayanov <bacco7@me.com>
2018-08-17 17:20:47 +0100

    06

---
+++ JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /06. Closures Lectures/Interview Question/main.js
@@ -0,0 +1,17 @@
+function interviewQuestions(job) {
+
+    return function(name) {
+
+        if (job === 'teacher'){
+            console.log(`What subject do you teach, ${name}.`);
+        } else if (job === 'designer') {
+            console.log(`${name}, can you please explain what UX design is?`);
+        } else {
+            console.log(`Hello ${name}, what do you do for leaving?`);
+        }
+    }
+}
+
+interviewQuestions('teacher')('John');
+interviewQuestions('designer')('Jame');
+interviewQuestions('singer')('Mark');

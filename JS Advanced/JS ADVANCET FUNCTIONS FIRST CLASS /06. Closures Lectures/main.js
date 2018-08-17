758f18e800f4d3e797f36f0ad3f698134657eab5
Veselin Trayanov <bacco7@me.com>
2018-08-17 16:09:30 +0100

    06

---
+++ JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /06. Closures Lectures/main.js
@@ -0,0 +1,19 @@
+let personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBurth)
+    }
+};
+ 
+let john = Object.create(personProto);
+
+    john.name ='John';
+    john.yearOfBurth =1990;
+    john.job ='teacher';
+
+var jane = Object.create(personProto,
+    {
+        name: { value: 'Jane'},
+        yearOfBurth: { value: 1969},
+        job: { value: 'designer'}
+    });
+    

50fa1bfb51d19bd49f7804efa491a6c8e1036faa
Veselin Trayanov <bacco7@me.com>
2018-08-17 16:43:23 +0100

    6

---
+++ JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /06. Closures Lectures/Retirement Age copy/main.js
@@ -0,0 +1,18 @@
+
+function retirement (retirementAge) {
+
+    let a =' years left until retirement.';
+
+    return function (yearOfBirth) {
+        let age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a );
+    }
+}
+
+//    retirementUs = retirement(66);  // USA
+//    retirementEu = retirement(65);  // EUROPE
+//    retirementIseland = retirement(67);  // ISELAND
+
+retirement(66)(1990)  //  USA      ->  40
+retirement(65)(1990)  //  EUROPE   ->  39
+retirement(67)(1990)  //  ISELAND  ->  41

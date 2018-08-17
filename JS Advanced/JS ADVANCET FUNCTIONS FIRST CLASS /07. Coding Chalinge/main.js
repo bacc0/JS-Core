f358816d9105d81f5e035be4dc1cc3dc4f79cd1c
Veselin Trayanov <bacco7@me.com>
2018-08-17 20:35:47 +0100

    7

--- JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /07. Coding Chalinge/main.js
+++ JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /07. Coding Chalinge/main.js
@@ -1,44 +1,46 @@
-function Qustion( qustion, answers, correct ) {
-    this.qustion = qustion;
-    this.answers = answers;
-    this.correct = correct;
-};
-
-Qustion.prototype.displayQuestion = function() {
-    console.log(this.qustion);
-
-    for (let i = 0; i < this.answers.length; i++) {
-        console.log(`${i}: ${this.answers[i]}`);
+(function () {
+    function Qustion( qustion, answers, correct ) {
+        this.qustion = qustion;
+        this.answers = answers;
+        this.correct = correct;
+    };
+    
+    Qustion.prototype.displayQuestion = function() {
+        console.log(this.qustion);
+    
+        for (let i = 0; i < this.answers.length; i++) {
+            console.log(`${i}: ${this.answers[i]}`);
+        }
     }
-}
-
-Qustion.prototype.checkAnswer = function(ans){
-    if(ans === this.correct){
-        alert('Correct Answer');
-    } else {
-        console.log('Wrong Answer');
+    
+    Qustion.prototype.checkAnswer = function(ans){
+        if(ans === this.correct){
+            alert('Correct Answer');
+        } else {
+            console.log('Wrong Answer');
+        }
     }
-}
-
-
-let q1 = new Qustion('Is Javascript the coolerst programming lenguage in the world?',
-['Yes', 'No'],
-0);
-
-let q2 = new Qustion("What is the name of this course's teacher?" ,
-['John', 'Michael', 'Jonas'],
-2);
-
-let q3 = new Qustion("What dose best describe coding?" ,
-['Boring', 'hard', 'Fun', 'Tedious'],
-2);
-
-let questions = [q1, q2, q3];
-
-let n = Math.floor(Math.random() * questions.length);
-
-questions[n].displayQuestion();
-
-let answer = parseInt(prompt('Please select the corect answer!'));
-
-questions[n].checkAnswer(answer);
+    
+    let q1 = new Qustion('Is Javascript the coolerst programming lenguage in the world?',
+    ['Yes', 'No'],
+    0);
+    
+    let q2 = new Qustion("What is the name of this course's teacher?" ,
+    ['John', 'Michael', 'Jonas'],
+    2);
+    
+    let q3 = new Qustion("What dose best describe coding?" ,
+    ['Boring', 'hard', 'Fun', 'Tedious'],
+    2);
+    
+    
+    let questions = [q1, q2, q3];
+    
+    let rand = Math.floor(Math.random() * questions.length);
+    
+    questions[rand].displayQuestion();
+    
+    let answer = parseInt(prompt('Please select the corect answer!'));
+    
+    questions[rand].checkAnswer(answer);
+})()

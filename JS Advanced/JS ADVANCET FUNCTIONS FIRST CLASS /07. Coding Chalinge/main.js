8d8eacd9f8efee96c6615332b532452796ee205a
Veselin Hr. Trayanov <bacco7@me.com>
2018-08-19 20:13:12 +0100

    Update main.js

--- JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /07. Coding Chalinge/main.js
+++ JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /07. Coding Chalinge/main.js
@@ -14,6 +14,7 @@ ______________SOLUTION ONE____________________
             console.log(`${i}: ${this.answers[i]}`);
         }
     }
+    
     Qustion.prototype.checkAnswer = function(ans){
         if(ans === this.correct){
             alert('Correct Answer');
@@ -45,6 +46,7 @@ ______________SOLUTION ONE____________________
     
     questions[rand].checkAnswer(answer);
 })();
+
 ______________SOLUTION TWO____________________
 */
 

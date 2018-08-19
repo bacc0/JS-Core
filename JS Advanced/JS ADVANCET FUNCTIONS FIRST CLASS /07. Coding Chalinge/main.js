a5cca108d07291a5a626be96daeae75486f3d233
Veselin Hr. Trayanov <bacco7@me.com>
2018-08-20 00:48:19 +0100

    Update main.js

--- JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /07. Coding Chalinge/main.js
+++ JS Advanced/JS ADVANCET FUNCTIONS FIRST CLASS /07. Coding Chalinge/main.js
@@ -3,54 +3,49 @@ ______________SOLUTION ONE____________________
 
 
 (function () {
-    function Question(question, answers, correct) {
+    function Question (question, answers, correct) {
+    this.question = question;
+    this.answers  = answers;
+    this.correct  = correct;
+}
 
-        this.question = question;
-        this.answers = answers;
-        this.correct = correct;
-    }
-    
-    Question.prototype.displayQuestion =
-        function () {
-            console.log(this.question);
-    
-            for (let i = 0; i < this.answers.length; i++) {
-                console.log(`${i}: ${this.answers[i]}`);
-            }
-        };
-    
-    Question.prototype.checkAnswer = 
-        function (correctAnswer) {
-            if (correctAnswer == this.correct){
-                console.log('Correct Answer !');
-            } else {
-                console.log('Warong Answer :)');
-            }
+Question.prototype.displayQuestion =
+    function () {
+        console.log(this.question);
+        
+        for (let i = 0; i < this.answers.length; i++) {
+            console.log(`${i}: ${this.answers[i]}`);
         }
-    
-    let q1 = new Question('Is Javascript the coolerst programming lenguage in the world?',
-            ['Yes', 'No', "I don't know!"],
-            0); 
-            
-    let q2 = new Question('What is the name of the teacher', 
-            ['John', 'Jane', 'Johnas'], 
-            2);
-            
-    let q3 = new Question('What does best describe coding?', 
-            ['boring', 'hard', 'fun', 'tediuos'], 
-            2);
-    
-    let questions = [q1, q2, q3 ];
-    
-    let random = Math.floor(
-                 Math.random() * questions.length);
-    
-    let currentQuestion = questions[random];
-    
-    
-    currentQuestion.displayQuestion();
-    let answer = prompt('Please corest answer');
-    currentQuestion.checkAnswer(answer);
+    }
+
+Question.prototype.checkTheAnswer =
+    function (answer) {   
+        if( answer === this.correct) {
+            console.log('👌🏽 Correct Answer 🦁');
+
+        } else {
+            console.log(' ☠️ Wrong Answer 💀');
+        }
+    }
+
+let q1 = new Question('Is Javascript the coolerst programming lenguage in the world?',
+        ['Yes', 'No', "I don't know!"],
+        0); 
+let q2 = new Question('What is the name of the teacher', 
+        ['John', 'Jane', 'Johnas'], 
+        2);
+let q3 = new Question('What does best describe coding?', 
+        ['boring', 'hard', 'fun', 'tediuos'], 
+        2);
+let arrQuestions = [q1, q2, q3];
+
+let randomNum = Math.floor( Math.random() * arrQuestions.length);
+
+arrQuestions[randomNum].displayQuestion();
+
+let answer = Number(prompt('Please answer the question! 😄'));
+
+arrQuestions[randomNum].checkTheAnswer(answer);
 })();
 
 ______________SOLUTION TWO____________________

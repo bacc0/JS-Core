f6c14d7aaeb23fad71b1ef477e1276a92b57a026
Veselin Trayanov <bacco7@me.com>
2018-08-20 15:41:36 +0100

    7

--- JS Advanced/A. Budgety App/app.js
+++ JS Advanced/A. Budgety App/app.js
@@ -7,16 +7,27 @@ let budgetController = (function () {
 
 // UI CONTROLLER
 let UIController = (function () {
+
+     let DOMstrings = {
+          inputType: '.add__type',
+          inputDescription: '.add__description',
+          inputValue: '.add__value',
+          inputBtn: '.add__btn'
+     }
      
      return {
           getInput: function(){
 
                return {
-                    type : document.querySelector('.add__type').value, // Will be ether inc or exp
-                    description : document.querySelector('.add__description').value,
-                    value : document.querySelector('.add__value').value
-
+                    type : document.querySelector(DOMstrings.inputType).value , // Will be ether inc or exp
+                    description : document.querySelector(DOMstrings.inputDescription).value ,
+                    value : document.querySelector(DOMstrings.inputValue).value
                }
+          },
+
+          getDOMstrings: function(){
+
+               return DOMstrings;
           }
      }
 })();
@@ -25,18 +36,21 @@ let UIController = (function () {
 // GLOBAL APP CONTROLLER
 let controller = (function (budgetCtrl, UICtrl) {
 
+     let DOM = UICtrl.getDOMstrings();
+
      let ctrlAddItem = function (){
           // 1. Get  the field input data
 
           let input = UICtrl.getInput();
           console.log(input);
+          
           // 2. Add the item to the budget controller
           // 3. Add the item to the UI
           // 4. Calculate the budget
           // 5. display the budget on the UI
      }
 
-     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+     document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
      document.addEventListener('keypress', function(event) {
           if (event.keyCode === 13 || event.which === 13){

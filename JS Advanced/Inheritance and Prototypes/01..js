77e8f1db992c2d660b068d96e085ef5809433c61
Veselin Trayanov <bacco7@me.com>
2018-08-23 22:39:04 +0100

    01

--- JS Advanced/Inheritance and Prototypes/01..js
+++ JS Advanced/Inheritance and Prototypes/01..js
@@ -1,16 +1,10 @@
-let commandProcessor = ( function() {
-    let text = '';
-    return {
-        append: (t) => text = text + t,
-        removeStart: (count) => text = text.slice(count),
-        removeEnd: (count) => text = text.slice(0, text.length - count),
-        print: function (  ) {
-            console.log(text);
-        }
-    };
-})();
+class Person {
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+    }
+}
 
-console.log(commandProcessor.append('pesho'));
-console.log(commandProcessor.removeStart(2));
-console.log(commandProcessor.removeEnd(2));
-
+class Teacher extends Person {
+    
+}
